42017e75d2d591d33543058e70e3a589f9c57260
John McKinnon <johnmckinnon23@yahoo.com>
2022-11-26 15:47:52 -0700

    commit tree 8f55d507895606fdf0355019149344fc86b05da2

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,18 @@
-class Student 
+require_relative './user'
+
+class Student < User
+
+  def initialize(knowledge=[])
+    @knowledge = knowledge
+
+  end
+
+  def learn(value)
+    @knowledge << value
+  end
+
+  def knowledge
+    @knowledge
+  end
 
 end
